49502c5d00beea12e03f4e7de5f8e3d42a9cf944
SonuKumar057 <sonukumar6204648@gmail.com>
2025-08-16 10:23:23 +0530

    initial commit

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -18,7 +18,7 @@ function Sidebar() {
 
     const getAllThreads = async () => {
         try {
-            const response = await fetch("http://localhost:8080/api/thread");
+            const response = await fetch("https://prompt-chat-ai-73po.vercel.app/api/thread");
             const res = await response.json();
             const filteredData = res.map(thread => ({
                 threadId: thread.threadId,
@@ -46,7 +46,7 @@ function Sidebar() {
         setCurrThreadId(newThreadId);
 
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const response = await fetch(`https://prompt-chat-ai-73po.vercel.app/api/thread/${newThreadId}`);
             const res = await response.json();
             setPrevChats(res);
             setNewChat(false);
@@ -58,7 +58,7 @@ function Sidebar() {
 
     const deleteThread = async (threadId) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {
+            const response = await fetch(`https://prompt-chat-ai-73po.vercel.app/api/thread/${threadId}`, {
                 method: "DELETE"
             });
             const res = await response.json();
